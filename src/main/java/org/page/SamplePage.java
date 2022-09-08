1fb4d1ad0a5f7c922bf5dca6004736e5951ab232
gurusiddharthan-v <gurusiddharthanv@gmail.com>
2022-09-08 22:54:08 +0530

    guru

--- src/main/java/org/page/SamplePage.java
+++ src/main/java/org/page/SamplePage.java
@@ -4,6 +4,10 @@ public class SamplePage {
 	
 	public static void main(String[] args) {
 		
+		System.out.println("test1");
+		System.out.println("test2");
+		System.out.println("test3");
+		
 	}
 
 }
